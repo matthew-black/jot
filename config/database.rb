de5dda05aac776435e4d13d1e2668852638a1c5e
Matthew Black <matthewallenblack@gmail.com>
2017-11-30 10:25:35 -0600

    Clean up comments/formatting.

--- config/database.rb
+++ config/database.rb
@@ -3,18 +3,9 @@ if Sinatra::Application.development?
   ActiveRecord::Base.logger = Logger.new(STDOUT)
 end
 
-# Automatically load every file in APP_ROOT/app/models/*.rb, e.g.,
-#   autoload "Person", 'app/models/person.rb'
-#
-# We have to do this in case we have models that inherit from each other.
-# If model Student inherits from model Person and app/models/student.rb is
-# required first, it will throw an error saying "Person" is undefined.
-#
-# With this lazy-loading technique, Ruby will try to load app/models/person.rb
-# the first time it sees "Person" and will only throw an exception if
-# that file doesn't define the Person class.
-#
-# See http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
+
+# Automatically loads all models:
+# http://www.rubyinside.com/ruby-techniques-revealed-autoload-1652.html
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   filename = File.basename(model_file).gsub('.rb', '')
   autoload ActiveSupport::Inflector.camelize(filename), model_file
--- config/environment.rb
+++ config/environment.rb
@@ -8,7 +8,6 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
-# Require gems we care about
 require 'rubygems'
 
 require 'uri'
@@ -30,38 +29,36 @@ end
 
 require 'erb'
 
-# Some helper constants for path-centric logic
+# Some helper constants for path-centric logic:
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
 APP_NAME = APP_ROOT.basename.to_s
 
 configure do
-  # By default, Sinatra assumes that the root is the file that calls the configure block.
-  # Since this is not the case for us, we set it manually.
+  # Sinatra assumes that the root is the file that calls the configure block.
+  # Thankfully....APP_ROOT :)
   set :root, APP_ROOT.to_path
-  # See: http://www.sinatrarb.com/faq.html#sessions
+
   enable :sessions
   set :session_secret, ENV['SESSION_SECRET'] || 'this is a secret shhhhh'
-
-  # Set the views to
   set :views, File.join(Sinatra::Application.root, "app", "views")
 end
 
-# Set up the controllers and helpers
+# Set up the controllers and helpers:
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
-# Include ApplicationRecord. This ports over the semantics seen in Rails 5 and
-# accustoms students to inheriting from ApplicationRecord.
+# Include ApplicationRecord:
 require APP_ROOT.join('app', 'models', 'application_record').to_s
 
-# Set up the database
+# Set up the database:
 require APP_ROOT.join('config', 'database')
 
-# Set up API tokens
+# Set up API tokens:
 Unsplash.configure do |config|
   config.application_id     = ENV["APPLICATION_ID"]
   config.application_secret = ENV["APPLICATION_SECRET"]
+  # Don't currently need this bit:
   # config.application_redirect_uri = "https://your-application.com/oauth/callback"
   config.utm_source = "jot"
 end
